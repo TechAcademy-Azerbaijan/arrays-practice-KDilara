4ef3e99d454e7803dd9e95e9402da37482f57e11
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-28 20:47:34 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,25 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  var input = result.input; // "5,3 5 6 8 9"
+  var arr = input.split(","); // ['5', '3 5 6 8 9']
+  let max = -Infinity;
+  let sum= 0;
+  let count;
+  for(i=0; i<arr.length; i++){
+    sum= sum+arr[i];
+    if( arr[i]>max){
+      max=arr[i]
+      count = 1;
+    }else if(arr[i]==max){
+      count ++
+    }
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  }
+
+  if(sum/arr.length < max){
+    console.log(max)
+   console.log(count)
+  }
 });
