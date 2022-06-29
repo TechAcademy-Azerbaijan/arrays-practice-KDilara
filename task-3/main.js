57e4d0febaaade869065e9cda9fad9ba7dc6e6df
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-29 11:22:49 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,30 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
+prompt.get("input", function (err, result) {
+  var input = result.input; // "5,3 5 6 8 9"
+  var arr = input.split(","); // ['5', '3 5 6 8 9']
+  let max = -Infinity;  
+    let min = Infinity;
+    let indexMax;
+    let indexMin;
+    for(i=0; i<arr.length; i++){
+      if( arr[i]>max){
+        max=arr[i]
+       indexMax=i;
+      }
+      if(arr[i]<min){
+        min = arr[i]
+        indexMin=i;
+      }
+    }
   
-    // Write code here
+    let temp = arr[indexMax];
+    arr[indexMax]=arr[indexMin];
+    arr[indexMin]=temp;
+    
+    for(i=0; i<arr.length; i++){
+  console.log(arr[i])
+    }
   
 });
