88667688abdb0cf5904b00ad9ca3bdf2f108c149
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-29 11:44:19 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -5,20 +5,23 @@ prompt.get("input", function (err, result) {
   var input = result.input; // "5,3 5 6 8 9"
   var arr = input.split(","); // ['5', '3 5 6 8 9']
   let max = -Infinity;
-  let sum = 0;
+  let sum= 0;
   let count;
-  for (i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-    if (arr[i] > max) {
-      max = arr[i];
+  for(i=0; i<arr.length; i++){
+    sum= sum+arr[i];
+    if( arr[i]>max){
+      max=arr[i]
       count = 1;
-    } else if (arr[i] == max) {
-      count++;
+    }else if(arr[i]==max){
+      count ++;
     }
+
   }
 
-  if (sum / arr.length < max) {
-    console.log(max * count);
-    console.log(count);
+  if(sum/arr.length < max){
+   
+    console.log(max*count)
+   console.log(count)
   }
+
 });
