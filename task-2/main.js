fe35751807b1ad855fe7965267df2e4ff35a5bc8
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-29 11:21:15 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,17 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  var input = result.input; // "5,3 5 6 8 9"
+  var arr = input.split(","); // ['5', '3 5 6 8 9']
+let count = 0;
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+for (i = 1; i < arr.length; i++) {
+  if(arr[1]!=undefined){
+  if (arr[i-1] < arr[i ] && arr[i ] > arr[i + 1]) {
+    count++;
+  }
+}
+}
+console.log(count);
 });
